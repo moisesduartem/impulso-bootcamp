8576f91a449d5276760cc18f3e36d60fa44aabff
Moisés Mariano <moises@softbuilder.com.br>
2021-02-14 23:28:26 -0300

    alguns metodos basicos para lidar com strings em ruby

---
+++ introducao/strings_2.rb
@@ -0,0 +1,9 @@
+puts "Moisés"[0] # exibe o caractere na posição 0 da string, M
+puts "Moisés"[0, 2] # exibe caracteres de posição 0 à 2
+puts "Moisés".chars # a string se torna um array e cada caractere um valor dele
+puts "-" * 10 # string com 10 hífens
+puts "          texto           ".strip # similar ao trim, limpa espaços ao redor
+puts "moi".upcase # tudo para maiúsculo
+puts "MOI".downcase # tudo para minúsculo
+puts "moisés".capitalize # coloca a primeira letra em maiúsculo
+puts "oi-tudo-bem".split("-") # retorna uma lista com itens dividos pela string fornecida
